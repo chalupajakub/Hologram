140b548fb0230675561ec071e1bce6749b98861c
Jakub Chalupa <chalupajakub57@gmail.com>
2025-05-19 21:12:20 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,32 @@
-<?php 
+<?php
+    include 'connect.php';
     session_start();
+
+    if (!isset($_SESSION['user_id'])) {
+    
+        header("Location: login.php");
+        exit;
+    }
+    
+    $id = $_SESSION['user_id'];
+    
+    $stmt = $conn->prepare
+    ("SELECT 
+    posts.image, 
+    posts.date, 
+    users.username, 
+    posts.content, 
+    users.pfp,
+    users.id
+    FROM posts
+    JOIN followers ON posts.user_id = followers.followed_id
+    JOIN users ON posts.user_id = users.id
+    WHERE followers.follower_id = ?
+    ORDER BY posts.date DESC
+    LIMIT 15");
+    
+    $stmt->bind_param("i", $id);
+    $stmt->execute();
 ?>
 
 <!DOCTYPE html>
@@ -15,80 +42,50 @@
 </head>
 <body>
     <header>
-        <a href="#" class="logo">Hologram</a>
+        <a href="index.php" class="logo">Hologram</a>
+        
+        <nav>
+            <div>
+                <a href="#" id="checked">Home</a>
+                <a href="post.php">Post</a>
+                <a href="profile.php?id=<?php echo $id?>">Profile</a>
+                <a href="#">Messages</a>
+            </div>
+        </nav>
+        
+        <div>
+            <a href="update-profile.php">Update profile</a>
+            <a href="logout.php" style="color: rgb(200, 100, 100)">Log out</a>
+        </div>
     </header>
 
-    <nav>
-        <div>
-            <a href="#" id="checked"><img src="images/home.png" alt="home" title="Home"></a>
-            <a href="#"><img src="images/upload.png" alt="upload" title="Upload"></a>
-            <a href="#"><img src="images/user.png" alt="profile" title="Profile"></a>
-        </div>
-    </nav>
-
-    <div class="people side-panel">
-        <h3>See others</h3>
-    </div>
-
-    <div class="profile side-panel">
-        <h3>Your profile</h3>
-
-        <img src="<?php echo $_SESSION['pfp']?>" alt="profile-pic">
-
-        <p id="name"><?php echo $_SESSION['username']?></p>
-        <p id="username"><?php echo $_SESSION['name']?></p>
-        <p id="bio"><?php echo $_SESSION['bio']?></p>
-
-        <a href="#" id="upload">
-            + Create post
-        </a>
-
-        <div class="settings">
-            <a href="update-profile.php">Update profile</a>
-            <a href="logout.php" style="color: rgb(200, 100, 100)">Log Out</a>
-        </div>
-    </div>
+    
 
     <main>
-        <div class="post">
+        <h3>Nejnovější příspěvky od lidí co sledujete</h3>
+        <?php 
+            $result = $stmt->get_result();
+            
+            while ($row = $result->fetch_assoc()) {
+                ?>
+                <div class="post">
             <div class="header">
-                <img src="images/pfp.jpg" alt="pfp">
-                <a href="#">marek_holomy</a>
+                <img src="<?php echo htmlspecialchars($row['pfp']);?>" alt="pfp">
+                <a href="profile.php?id=<?php echo htmlspecialchars($row['id']);?>"><?php echo htmlspecialchars($row['username']);?></a>
             </div>
             <div class="content">
-                <img src="images/fotka.png" alt="post">
+                <img src="<?php echo htmlspecialchars($row['image']); ?>" alt="post">
             </div>
             <div class="footer">
                 <p>
-                    Lorem ipsum dolor sit amet consectetur, adipisicing elit. Est nulla tenetur earum voluptates maxime veniam corrupti laudantium impedit aut. Voluptatibus incidunt error commodi eaque ipsa asperiores! Laudantium, hic. Totam, molestias.
+                    <?php echo htmlspecialchars($row['content']); ?>
                 </p>
             </div>
         </div>
-
-        <div class="post">
-            <div class="header">
-                <img src="images/pfp.jpg" alt="pfp">
-                <a href="#">marek_holomy</a>
-            </div>
-            <div class="content">
-                <img src="images/fotka.png" alt="post">
-            </div>
-            <div class="footer">
-                <p>
-                    Lorem ipsum dolor sit amet consectetur, adipisicing elit. Est nulla tenetur earum voluptates maxime veniam corrupti laudantium impedit aut. Voluptatibus incidunt error commodi eaque ipsa asperiores! Laudantium, hic. Totam, molestias.
-                </p>
-            </div>
-        </div>
+        <?php
+            }
+        ?>
+        
     </main>
 </body>
 </html>
-
-<?php
-    session_start();
-
-    if (!isset($_SESSION['user_id'])) {
-    
-        header("Location: login.php");
-        exit;
-    }
-?>
