c7f8a9ee966b1067b2b4870ff23ff9ef40e65526
Jakub Chalupa <chalupajakub57@gmail.com>
2025-06-11 12:04:56 +0200

    Update get_followers.php

--- get_followers.php
+++ get_followers.php
@@ -1,1 +1,27 @@
+<?php
+include 'connect.php';
+header('Content-Type: application/json');
 
+if (!isset($_GET['user_id'])) {
+    die(json_encode([]));
+}
+
+$userId = $_GET['user_id'];
+
+$query = "SELECT users.id, users.username, users.name, users.pfp 
+          FROM followers 
+          JOIN users ON followers.follower_id = users.id 
+          WHERE followers.followed_id = ?";
+
+$stmt = $conn->prepare($query);
+$stmt->bind_param("i", $userId);
+$stmt->execute();
+$result = $stmt->get_result();
+
+$followers = [];
+while ($row = $result->fetch_assoc()) {
+    $followers[] = $row;
+}
+
+echo json_encode($followers);
+?>
